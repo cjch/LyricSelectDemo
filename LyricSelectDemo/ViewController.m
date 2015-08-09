d019ae525e7a9547647b3d9ecb8dd4fff3201d32
= <=>
2015-08-09 10:31:00 +0800

    create repository

---
+++ LyricSelectDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LyricSelectDemo
+//
+//  Created by chenjie on 15/8/9.
+//  Copyright (c) 2015年 jch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
